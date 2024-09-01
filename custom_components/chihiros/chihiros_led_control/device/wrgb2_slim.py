4167d60118f0970af91147d665b3821ed2d10494
Michael Dietrich <udvjw@student.kit.edu>
2024-09-01 12:28:35 +0200

    remove white channel from wrgb 2 slim

--- custom_components/chihiros/chihiros_led_control/device/wrgb2_slim.py
+++ custom_components/chihiros/chihiros_led_control/device/wrgb2_slim.py
@@ -9,7 +9,6 @@ class WRGBIISlim(BaseDevice):
     _model_name = "WRGB II Slim"
     _model_codes = ["DYSILN"]
     _colors: dict[str, int] = {
-        "white": 3,
         "red": 0,
         "green": 1,
         "blue": 2,
