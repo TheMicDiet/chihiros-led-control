5824a4657bfa92b8a052b994024bbea6ae6d899e
Michael Dietrich <github@mi-dietrich.de>
2025-04-12 09:45:18 +0000

    add additional model code for z light tiny

--- custom_components/chihiros/chihiros_led_control/device/z_light_tiny.py
+++ custom_components/chihiros/chihiros_led_control/device/z_light_tiny.py
@@ -7,7 +7,7 @@ class ZLightTiny(BaseDevice):
     """Z Light TINY device Class."""
 
     _model_name = "Z Light TINY"
-    _model_codes = ["DYSSD"]
+    _model_codes = ["DYSSD", "DYZSD"]
     _colors: dict[str, int] = {
         "white": 0,
         "warm": 1,
