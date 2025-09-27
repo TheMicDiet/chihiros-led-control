0a9fcea5bfd33102a0c30b405d08dade506dc8dc
Michael Dietrich <udvjw@student.kit.edu>
2025-09-27 09:55:08 +0200

    add more model codes for c2rgb and wrgb2 slim

--- custom_components/chihiros/chihiros_led_control/device/c2rgb.py
+++ custom_components/chihiros/chihiros_led_control/device/c2rgb.py
@@ -7,7 +7,7 @@ class CIIRGB(BaseDevice):
     """Chihiros CII RGB device Class."""
 
     _model_name = "C II RGB"
-    _model_codes = ["DYNCRGP"]
+    _model_codes = ["DYNCRGP", "DYNCRGB"]
     _colors: dict[str, int] = {
         "red": 0,
         "green": 1,
--- custom_components/chihiros/chihiros_led_control/device/wrgb2_slim.py
+++ custom_components/chihiros/chihiros_led_control/device/wrgb2_slim.py
@@ -7,7 +7,7 @@ class WRGBIISlim(BaseDevice):
     """Chihiros WRGB II Slim device Class."""
 
     _model_name = "WRGB II Slim"
-    _model_codes = ["DYSILN"]
+    _model_codes = ["DYSILN", "DYSL30", "DYSL45", "DYSL60", "DYSL90", "DYSL120"]
     _colors: dict[str, int] = {
         "red": 0,
         "green": 1,
